a69ad11c595e7b671822a38466c889e17ec826d3
Olesia Vysotina <vysotina2002@gmail.com>
2026-02-08 19:50:03 +0100

    hotfix: add ScrollToTop and fix header background-color and section spacing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 
 import App from "./App.jsx";
 import "modern-normalize";
@@ -8,6 +9,7 @@ import "modern-normalize";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
